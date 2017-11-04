63e887c61e384232375fa2d82b5f2bff9d17f369
EvilEpicCoder <1st.elixile@gmail.com>
2017-11-04 05:49:11 +0300

    second commit

--- EEC_timeOfRead.js
+++ EEC_timeOfRead.js
@@ -3,3 +3,22 @@
 * read of the article.              *
 * EEC_timeOfRead by EEC             *
 ************************************/
+var jEEC_tfr={
+ 'inID':'textForCalc',
+ 'outID':'calcTime',
+ 'avReadPerWord':1,
+ 'arrWordForCalc':[],
+ 'initTFR':function (){
+   console.log('start HERE');
+   this.arrWordForCalc=document.getElementById(this.inID);// [This is first art]
+   var tExtract=document.getElementById(this.outID);//[field1]
+   var arr=this.arrWordForCalc.value.split(' ');
+   var sec=((arr.length*this.avReadPerWord)%60);
+   tExtract.value="average: "+sec+"sec read";
+ }
+};
+window.onload=function(){
+  console.log('start');
+  var a=new Object(jEEC_tfr);
+  a.initTFR();
+}
