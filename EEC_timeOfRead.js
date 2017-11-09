8407427bc8a60a9aea7331b54230136030530b98
EvilEpicCoder <1st.elixile@gmail.com>
2017-11-09 17:31:32 +0300

    beta, fully working utility

--- EEC_timeOfRead.js
+++ EEC_timeOfRead.js
@@ -6,19 +6,57 @@
 var jEEC_tfr={
  'inID':'textForCalc',
  'outID':'calcTime',
- 'avReadPerWord':1,
+ 'avReadPerWord':0.4,
  'arrWordForCalc':[],
- 'initTFR':function (){
+ 'initTFR':()=>{
    console.log('start HERE');
-   this.arrWordForCalc=document.getElementById(this.inID);// [This is first art]
-   var tExtract=document.getElementById(this.outID);//[field1]
-   var arr=this.arrWordForCalc.value.split(' ');
-   var sec=((arr.length*this.avReadPerWord)%60);
-   tExtract.value="average: "+sec+"sec read";
+   jEEC_tfr.arrWordForCalc=document.getElementById(jEEC_tfr.inID);// [This is first art]
+   const tExtract=document.getElementById(jEEC_tfr.outID);//[field1]
+   const arr=jEEC_tfr.arrWordForCalc.value.split(' ');
+   let years, months, days, hours, minutes, seconds;
+   let finalString="";
+   seconds=parseInt(jEEC_tfr.avReadPerWord * arr.length);
+   console.log(arr.length);
+     if(seconds%31104000 && seconds>31104000)
+     years=parseInt(seconds/31104000);//(12*30*24*60*60);
+     seconds=seconds%31104000;
+     if(seconds%2592000&&seconds>2592000)
+     months=parseInt(seconds/2592000); //(30*24*60*60);
+     seconds=seconds%2592000;
+     if(seconds%86400&&seconds>86400)
+     days=parseInt(seconds/86400);     //(24*60*60);
+     seconds=seconds%86400;
+     if(seconds%3600&&seconds>3600)
+     hours=parseInt(seconds/3600);     //(60*60);
+     seconds=seconds%3600;
+     if(seconds%60&&seconds>60)
+     minutes=parseInt(seconds/60);     //(60)
+     seconds=seconds%60;
+    console.log(years+" years");
+    console.log(months+" months");
+    console.log(days+" days");
+    console.log(minutes+" minutes");
+    console.log(seconds+" seconds");
+
+  if(years!==0&&years!==undefined)
+  finalString+=years+" y, ";
+  if(months!==0&&months!==undefined)
+  finalString+=months+" m,";
+  if(days!==0&&days!==undefined)
+  finalString+=days+" d, ";
+  if(hours!==0&&hours!==undefined)
+  finalString+=hours+" h, ";
+  if(minutes!==0&&minutes!==undefined)
+  finalString+=minutes+" min, ";
+  if(seconds!==0&&seconds!==undefined)
+  finalString+=seconds+" sec";
+   tExtract.value=finalString;
  }
 };
 window.onload=function(){
   console.log('start');
   var a=new Object(jEEC_tfr);
   a.initTFR();
+  const button=document.getElementById("button");
+  button.addEventListener("click",a.initTFR,false);
 }
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ## EEC_timeOfRead
-### Article time read helper
+### Article time read utility
 
 #### 1.Project
  * Project created for Plug and Play script
  * Why it created, which problens solved: Calculates information about required time to read article
  * Small story telling : Handy to have
 #### 2. What I use in this project
- * Frameworks : JavaScript
- * Links: [This is Link CHANGE_HERE name](https://www.CHANGE_HERE)
- * What cool things I found:
-  * What I do not like:
-  * Features: Easy to integrate
+ * Frameworks : JavaScript, OOP
+ * Links: [Screenshot](https://github.com/EvilEpicCoder/EEC_timeOfRead/blob/master/screenshot.jpg)
+ * What cool things I found: Very simple, cross platform.
+  * What I do not like: Very simple script.
+  * Features: Easy to use
 #### 3. My thoughts
-  * Pros and Cons :
-  * Time consumption
-  * What I learned
+  * Pros and Cons : You can use with any platform
+  * Time consumption: I spend few days, wrote on es6-7 and rewrite to es5, because es6-7 still not all supported.
+  * What I learned, still required to use Babel or another transpiler.
 
-  Version: `0.3a`
-  Date: `6.11.2017`
+  Version: `0.4b`
+  Date: `9.11.2017`
